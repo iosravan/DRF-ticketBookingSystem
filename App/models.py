d4c99d6868d7dd4ef7fb23b011a246c7c66c1138
Sravan Reddy <41821183+iosravan@users.noreply.github.com>
2022-05-24 14:04:50 +0530

    Update models.py

--- App/models.py
+++ App/models.py
@@ -2,10 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 # Create your models here.
-class User_info(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='self')
-    checker_level = models.CharField(max_length=100, null=True, blank=True)
-    gender = models.CharField(max_length=10, null=True, blank=True)
 
 class Tickets(models.Model):
     movie_id = models.CharField(max_length=50,primary_key=True,default='M-1001')
